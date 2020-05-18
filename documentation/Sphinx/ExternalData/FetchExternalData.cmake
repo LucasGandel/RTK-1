ebb301ab26e6ed3202a2f06f1b3ed6a2966bc29b
Lucas Gandel <lucas.gandel@kitware.com>
2020-05-18 18:23:09 +0200

    WIP: Fix doc

--- documentation/Sphinx/ExternalData/FetchExternalData.cmake
+++ documentation/Sphinx/ExternalData/FetchExternalData.cmake
@@ -15,12 +15,15 @@ endif()
 file(GLOB_RECURSE content_files
   "${EXTERNAL_DATA_INPUT_ROOT}/*${link_content}")
 
+message(STATUS "Downloading ${content_files}")
+
+
 foreach(content_file ${content_files})
 
   # Skip test inputs and baselines
-  if("${content_file}" MATCHES "test")
-    continue()
-  endif()
+  # if("${content_file}" MATCHES "test") ##########################
+    # continue()
+  # endif()
   
   file(RELATIVE_PATH content_file_rel_path ${EXTERNAL_DATA_INPUT_ROOT} ${content_file})
   get_filename_component(last_ext "${content_file_rel_path}" LAST_EXT)
@@ -30,6 +33,10 @@ foreach(content_file ${content_files})
   set(URL "https://data.kitware.com:443/api/v1/file/hashsum/${link_content}/${hash}/download")
   string(REGEX REPLACE "\n" "" URL "${URL}")
   file(DOWNLOAD "${URL}" "${EXTERNAL_DATA_OUTPUT_ROOT}/${content_file_rel_path}")
+  message(STATUS "Downloaded ${EXTERNAL_DATA_OUTPUT_ROOT}/${content_file_rel_path}")
 
   
 endforeach()
+
+message(STATUS "EXTERNAL_DATA_INPUT_ROOT:${EXTERNAL_DATA_INPUT_ROOT}")
+message(STATUS "EXTERNAL_DATA_OUTPUT_ROOT:${EXTERNAL_DATA_OUTPUT_ROOT}")
